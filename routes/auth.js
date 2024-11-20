219711feebf578823621030ae493751d2bc6958d
JANAKIRAMAN <janakiramankeerthivelan1021@gmail.com>
2024-11-20 10:50:48 +0530

    Replaced bcrypt with bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken'); // Import jsonwebtoken
 const User = require('../models/User'); // Import the User model
 const router = express.Router();
